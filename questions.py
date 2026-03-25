946f3d52be29d703d0bb199128bea68f25cbefb0
Manuel Rodriguez <manuelrodriguezunlp@gmail.com>
2026-03-25 19:38:26 -0300

    Modifique el juego, permitiendo al usuario por jugar mas de una ronda. Con la utilizacion del sample, pude resolver lo solicitado por el enunciado, poder jugar varias rondas sin que se repitan, las palabras a adivinar. El sistema de puntuacion tambien fue modificado, ya que antes devolvia un puntaje al finalizar una ronda. Ahora el puntaje se va acumulando ronda tras ronda, y al final de la partida, se le informa al Jugador cual fue su puntuacion final entre todas las rondas jugadas. El sistema de puntuacion quedo de la siguiente manera: La valoracion mas alta que puede obtener el jugador es 18, y la mas baja sigue siendo 0, que es cuando no adivino, en ninguna de las rondas. Sistema de rondas: Las rondas a jugar, son seleccionadas por el jugador, previamente se le avisa cual es el maximo de rondas que se pueden jugar, dada la categoria escogida. El usuario escoge entre el maximo y el minimo que es 1, cabe aclarar que el programa, se asegura de que el usuario no ingrese una cantidad de rondas no compatibles con el rango.

--- questions.py
+++ questions.py
@@ -5,14 +5,11 @@ words = {
     "Términos de Programación": ["programa", "variable", "funcion", "bucle"],
     "Tipos de Datos": ["entero", "lista", "cadena"],
 }
-guessed = []
-attempts = 6  # intentos
-score = 0
+
 print("¡Bienvenido al Ahorcado!")
 print()
 
 # Solicito al usario que elija una categoría.
-
 category = int(
     input(
         "Ingrese una cateogoria: 1. Nombres de Lenguajes de Programación, 2. Términos de Programación, 3. Tipos de Datos: "
@@ -25,44 +22,79 @@ elif category == 2:
 else:
     chosen_category = "Tipos de Datos"
 
-# Elijo una palabra al azar de la categoría seleccionada por el usuario
-word = random.choice(words[chosen_category])
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""  # por cada intento borra el progreso anterior, para luego comprobar atraves de si se agrega o no el guion, si adivino la palabra
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
+# Pongo todas las palabra de la categoría seleccionada por el usuario en una variable para luego hacer el sample
+words_bag = words[chosen_category]
+
+# Seteo el maximo de rondas que se puden jugar en esa categoria, dada las palabras que contiene esta.
+max_rounds_to_play = len(words_bag)
+
+# Solicito al usuario que elija cuantas rondas quiere jugar entre 1 y el maximo de rondas que se pueden jugar en esa categoria.
+rounds_to_play = int(
+    input(
+        f"¿Cuántas rondas querés jugar?, Como maximo podes jugar {max_rounds_to_play} rondas "
+    )
+)
+while (
+    rounds_to_play < 1 or rounds_to_play > max_rounds_to_play
+):  # Me aseguro que el usuario ingrese un numero entre 1 y el maximo de rondas que se pueden jugar en la categorai escogida.
+    rounds_to_play = int(
+        input(
+            f" Entrada no valida!!! Por favor, ingrese un número entre 1 y {max_rounds_to_play}: "
+        )
+    )
+selected_words = random.sample(
+    words_bag, rounds_to_play
+)  # Elijo las palabras al azar de la categoria seleccionada por el usuario, sin que se repitan, y las guardo en una lista.
+final_score = 0
+for i in range(rounds_to_play):
+    guessed = []
+    attempts = 6  # intentos
+    round_scoring = 0
+    word = selected_words[
+        i
+    ]  # Seleccion aleatoramiente la palabra a adivinar en esa ronda.
+    print(
+        f" Comienza la ronda {i + 1} de {rounds_to_play} "
+    )  # Informo al usuario que numero de ronda esta jugando y cauntas restan por jugar.
+    while attempts > 0:
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""  # por cada intento borra el progreso anterior, para luego comprobar atraves de si se agrega o no el guion, si adivino la palabra
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        print(progress)
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print("¡Ganaste!")
+            round_scoring += 6
+            break
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}")
+        letter = input("Ingresá una letra: ")
+        if (
+            len(letter) != 1 or not letter.isalpha()
+        ):  # Me aseguro que el usuario ingrese solo una letra y que sea alfabética
+            print("Entrada no válida")
+            continue
+        if (
+            letter in guessed
+        ):  # evaluo si la letra esta en la lista de las letras ya ingresadas
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
         else:
-            progress += "_ "
-    print(progress)
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        score += 6
-        break
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-    letter = input("Ingresá una letra: ")
-    if (
-        len(letter) != 1 or not letter.isalpha()
-    ):  # Me aseguro que el usuario ingrese solo una letra y que sea alfabética
-        print("Entrada no válida")
-        continue
-    if (
-        letter in guessed
-    ):  # evaluo si la letra esta en la lista de las letras ya ingresadas
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
+            guessed.append(letter)
+            attempts -= 1
+            round_scoring -= 1
+            print("Esa letra no está en la palabra.")
+        print()
     else:
-        guessed.append(letter)
-        attempts -= 1
-        score -= 1
-        print("Esa letra no está en la palabra.")
-    print()
-else:
-    print(f"¡Perdiste! La palabra era: {word}")
-    score = 0
-print(f"Tu puntaje final es: {score}")
+        print(f"¡Perdiste! La palabra era: {word}")
+        round_scoring = 0
+    final_score += round_scoring
+print(
+    f"Tu puntaje final es: {final_score}"
+)  # El score final se calcula sumando la puntuacion de cada ronda.
